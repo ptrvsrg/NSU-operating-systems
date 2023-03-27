100dd12e47ed3a14fb48b7b84bc3f4071e29cfe8
ptrvsrg <s.petrov1@g.nsu.ru>
2023-03-27 10:24:48 +0700

    Update

--- system_calls/task1/hello_syscall.c
+++ system_calls/task1/hello_syscall.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <sys/syscall.h>
 #include <unistd.h>
 
@@ -5,18 +6,25 @@ ssize_t my_write(unsigned int fd, const char* buf, size_t len);
 
 int main(int argc, char** argv)
 {
-	ssize_t ret;
-	char buf[] = "Hello, World!\n";
+    char buf[] = "Hello, World!\n";
+    ssize_t ret;
+    ssize_t written = 0;
+    ssize_t len = sizeof(buf);
 
-	ret = my_write(STDOUT_FILENO, buf, sizeof(buf));
-	
-	if (ret != sizeof(buf))
-		return -1;
+    while (written < len)
+    {
+        ret = my_write(STDOUT_FILENO, buf + written, len - written);
 
-	return 0;
+        if (ret == -1)
+            return EXIT_FAILURE;
+
+        written += ret;
+    }
+
+    return EXIT_SUCCESS;
 }
 
-ssize_t my_write(unsigned int fd, const char* buf, size_t len) 
+ssize_t my_write(unsigned int fd, const char* buf, size_t len)
 {
     return syscall(SYS_write, fd, buf, len);
 }
--- system_calls/task1/hello_write.c
+++ system_calls/task1/hello_write.c
@@ -1,14 +1,22 @@
+#include <stdlib.h>
 #include <unistd.h>
 
 int main(int argc, char** argv)
 {
-	ssize_t ret;
-	char buf[] = "Hello, World!\n";
-	
-	ret = write(STDOUT_FILENO, buf, sizeof(buf));
-	
-	if (ret != sizeof(buf))
-		return -1;
+    char buf[] = "Hello, World!\n";
+    ssize_t ret;
+    ssize_t written = 0;
+    ssize_t len = sizeof(buf);
 
-	return 0;
+    while (written < len) 
+    {
+        ret = write(STDOUT_FILENO, buf + written, len - written);
+
+        if (ret == -1)
+            return EXIT_FAILURE;
+
+        written += ret;
+    }
+
+    return EXIT_SUCCESS;
 }
