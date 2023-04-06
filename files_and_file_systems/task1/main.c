4dba51420cd9a11c45a94d6b7d05aa2c9d4feb51
ptrvsrg <s.petrov1@g.nsu.ru>
2023-04-06 18:02:30 +0700

    Update

--- files_and_file_systems/task1/main.c
+++ files_and_file_systems/task1/main.c
@@ -1,5 +1,4 @@
 #include <dirent.h>
-#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -9,56 +8,42 @@
 #define ERROR (-1)
 #define BUFFER_MAX 256
 
-void print_error(const char *file_path);
 long max(long x, long y);
-void reverse_string(char *dest, const char *src);
+void print_usage(const char *program_name);
+void reverse_string(char *dest, const char *src, int dest_length);
+void split_file_path(const char *file_path, char *working_dir, char *file_name);
+void join_file_path(const char *working_dir, const char *file_name, char *file_path);
+int process_command_line_args(int argc, char **argv);
+int create_reverse_dir(const char *dir_path, const char *reverse_dir_path);
+int process_dir_entry(const struct dirent *dir_entry, const char *dest_working_dir, const char *src_working_dir);
 long get_file_size(FILE *file);
 int read_file(char *buffer, size_t size, FILE *in_stream);
 int write_file(const char *buffer, size_t size, FILE *in_stream);
-int create_reverse_dir(const char *working_dir, const char *dir_name);
-int create_reverse_file(const char *dest_working_dir, const char *src_working_dir, const char *file_name);
+int close_file_streams(FILE *dest_stream, FILE *src_stream);
+int create_reverse_file(const char *file_path, const char *reverse_file_path);
 
 int main(int argc, char **argv) {
-    char working_dir[PATH_MAX] = {};
-    char dir_name[NAME_MAX] = {};
-    char *last_slash = NULL;
-
-    if (argc < 2) {
-        printf("Usage: %s DIRECTORY...\n", argv[0]);
+    int ret = process_command_line_args(argc, argv);
+    if (ret == ERROR)
         return EXIT_FAILURE;
-    }
-
-    for (int i = 1; i < argc; ++i) {
-        last_slash = strrchr(argv[i], '/');
-        if (last_slash != NULL) {
-            strncpy(working_dir, argv[i], last_slash - argv[i] + 1);
-            strcpy(dir_name, last_slash + 1);
-        } else {
-            strcpy(working_dir, "./");
-            strcpy(dir_name, argv[i]);
-        }
-
-        if (create_reverse_dir(working_dir, dir_name) == ERROR)
-            return EXIT_FAILURE;
-    }
 
     return EXIT_SUCCESS;
-}
-
-void print_error(const char *file_path) {
-    fprintf(stderr, "%s : %s\n", file_path, strerror(errno));
 }
 
 long max(long x, long y) {
     return (x <= y) ? y : x;
 }
 
-void reverse_string(char *dest, const char *src) {
+void print_usage(const char *program_name) {
+    printf("Usage: %s dir...\n", program_name);
+}
+
+void reverse_string(char *dest, const char *src, int dest_length) {
     char temp;
     char *begin = NULL;
     char *end = NULL;
 
-    strcpy(dest, src);
+    strncpy(dest, src, dest_length);
     begin = dest;
     end = dest + strlen(dest) - 1;
 
@@ -71,33 +56,132 @@ void reverse_string(char *dest, const char *src) {
     }
 }
 
+void split_file_path(const char *file_path, char *working_dir, char *file_name) {
+    char *last_slash = strrchr(file_path, '/');
+
+    if (last_slash == NULL) {
+        strncpy(working_dir, ".", PATH_MAX);
+        strncpy(file_name, file_path, NAME_MAX);
+    } else {
+        strncpy(working_dir, file_path, last_slash - file_path);
+        strncpy(file_name, last_slash + 1, NAME_MAX);
+    }
+}
+
+void join_file_path(const char *working_dir, const char *file_name, char *file_path) {
+    snprintf(file_path, PATH_MAX, "%s/%s", working_dir, file_name);
+}
+
+int process_command_line_args(int argc, char **argv) {
+    int ret = SUCCESS;
+    char working_dir[PATH_MAX + 1] = {};
+    char dir_name[NAME_MAX + 1] = {};
+    char reverse_dir_name[NAME_MAX + 1] = {};
+    char reverse_dir_path[PATH_MAX + 1] = {};
+
+    if (argc < 2) {
+        print_usage(argv[0]);
+        return ERROR;
+    }
+
+    for (int i = 1; i < argc; ++i) {
+        split_file_path(argv[i], working_dir, dir_name);
+
+        reverse_string(reverse_dir_name, dir_name, NAME_MAX);
+        join_file_path(working_dir, reverse_dir_name, reverse_dir_path);
+
+        ret = create_reverse_dir(argv[i], reverse_dir_path);
+        if (ret == ERROR)
+            return ERROR;
+    }
+
+    return SUCCESS;
+}
+
+int create_reverse_dir(const char *dir_path, const char *reverse_dir_path) {
+    int ret = SUCCESS;
+    DIR *dir_stream = NULL;
+    struct dirent *dir_entry = NULL;
+
+    dir_stream = opendir(dir_path);
+    if (dir_stream == NULL) {
+        perror(dir_path);
+        return ERROR;
+    }
+
+    ret = mkdir(reverse_dir_path, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    if (ret == ERROR) {
+        perror(reverse_dir_path);
+        return ERROR;
+    }
+
+    dir_entry = readdir(dir_stream);
+    while (dir_entry != NULL) {
+        ret = process_dir_entry(dir_entry, dir_path, reverse_dir_path);
+        if (ret == ERROR) {
+            closedir(dir_stream);
+            return ERROR;
+        }
+
+        dir_entry = readdir(dir_stream);
+    }
+
+    closedir(dir_stream);
+    return SUCCESS;
+}
+
+int process_dir_entry(const struct dirent *dir_entry, const char *dest_working_dir, const char *src_working_dir) {
+    int ret = SUCCESS;
+    char file_path[PATH_MAX + 1] = {};
+    char reverse_file_name[NAME_MAX + 1] = {};
+    char reverse_file_path[PATH_MAX + 1] = {};
+
+    join_file_path(dest_working_dir, dir_entry->d_name, file_path);
+    reverse_string(reverse_file_name, dir_entry->d_name, NAME_MAX);
+    join_file_path(src_working_dir, reverse_file_name, reverse_file_path);
+
+    if (dir_entry->d_type == DT_REG)
+        ret = create_reverse_file(file_path, reverse_file_path);
+    else if (dir_entry->d_type == DT_DIR && strcmp(dir_entry->d_name, ".") != 0 && strcmp(dir_entry->d_name, "..") != 0)
+        ret = create_reverse_dir(file_path, reverse_file_path);
+
+    return ret;
+}
+
 long get_file_size(FILE *file) {
+    int ret = SUCCESS;
     long begin_pos = 0;
     long size = 0;
 
-    if ((begin_pos = ftell(file)) == ERROR)
+    begin_pos = ftell(file);
+    if (begin_pos == ERROR)
         return ERROR;
 
-    if (fseek(file, 0, SEEK_END) == ERROR)
+    ret = fseek(file, 0, SEEK_END);
+    if (ret == ERROR)
         return ERROR;
 
-    if ((size = ftell(file)) == ERROR)
+    size = ftell(file);
+    if (size == ERROR)
         return ERROR;
 
-    if (fseek(file, begin_pos, SEEK_SET) == ERROR)
+    ret = fseek(file, begin_pos, SEEK_SET);
+    if (ret == ERROR)
         return ERROR;
 
     return size;
 }
 
 int read_file(char *buffer, size_t size, FILE *in_stream) {
+    int ret = SUCCESS;
     size_t read = 0;
     size_t total_read = 0;
 
     while (total_read < size) {
         read = fread(buffer + total_read, sizeof(char), size - total_read, in_stream);
 
-        if (read == 0 && ferror(in_stream) != SUCCESS)
+        ret = ferror(in_stream);
+        if (read == 0 && ret != SUCCESS)
             return ERROR;
 
         total_read += read;
@@ -107,13 +191,15 @@ int read_file(char *buffer, size_t size, FILE *in_stream) {
 }
 
 int write_file(const char *buffer, size_t size, FILE *in_stream) {
+    int ret = SUCCESS;
     size_t written = 0;
     size_t total_written = 0;
 
     while (total_written < size) {
         written = fwrite(buffer + total_written, sizeof(char), size - total_written, in_stream);
 
-        if (written == 0 && ferror(in_stream) != SUCCESS)
+        ret = ferror(in_stream);
+        if (written == 0 && ret != SUCCESS)
             return ERROR;
 
         total_written += written;
@@ -122,75 +208,39 @@ int write_file(const char *buffer, size_t size, FILE *in_stream) {
     return SUCCESS;
 }
 
-int create_reverse_dir(const char *working_dir, const char *dir_name) {
-    char dir_path[PATH_MAX] = {};
-    char reverse_dir_name[NAME_MAX] = {};
-    char reverse_dir_path[PATH_MAX] = {};
-    DIR *dir = NULL;
-    struct dirent *entry = NULL;
+int close_file_streams(FILE *dest_stream, FILE *src_stream) {
+    if (dest_stream != NULL)
+        fclose(dest_stream);
 
-    sprintf(dir_path, "%s%s", working_dir, dir_name);
-
-    reverse_string(reverse_dir_name, dir_name);
-    sprintf(reverse_dir_path, "%s%s", working_dir, reverse_dir_name);
-
-    dir = opendir(dir_path);
-    if (dir == NULL) {
-        print_error(dir_path);
-        return ERROR;
-    }
-
-    if (mkdir(reverse_dir_path, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH) == ERROR) {
-        print_error(reverse_dir_path);
-        return ERROR;
-    }
-
-    while ((entry = readdir(dir)) != NULL) {
-        if (entry->d_type == DT_REG) {
-            if (create_reverse_file(dir_path, reverse_dir_path, entry->d_name) == ERROR) {
-                closedir(dir);
-                return ERROR;
-            }
-        }
-    }
-
-    closedir(dir);
-    return SUCCESS;
+    if (src_stream != NULL)
+        fclose(src_stream);
 }
 
-int create_reverse_file(const char *dest_working_dir, const char *src_working_dir, const char *file_name) {
+int create_reverse_file(const char *file_path, const char *reverse_file_path) {
+    int ret = SUCCESS;
     size_t reversed = 0;
     size_t file_size = 0;
-    char file_path[PATH_MAX] = {};
-    char reverse_file_path[PATH_MAX] = {};
-    char reverse_file_name[NAME_MAX] = {};
-    char buffer[BUFFER_MAX] = {};
+    char buffer[BUFFER_MAX + 1] = {};
     FILE *src_stream = NULL;
     FILE *dest_stream = NULL;
 
-    sprintf(file_path, "%s/%s", dest_working_dir, file_name);
-
-    reverse_string(reverse_file_name, file_name);
-    sprintf(reverse_file_path, "%s/%s", src_working_dir, reverse_file_name);
-
-    src_stream = fopen(file_path, "rb");
+    src_stream = fopen(file_path, "r");
     if (src_stream == NULL) {
-        print_error(file_path);
+        perror(file_path);
         return ERROR;
     }
 
-    dest_stream = fopen(reverse_file_path, "wb");
+    dest_stream = fopen(reverse_file_path, "w");
     if (dest_stream == NULL) {
+        perror(reverse_file_path);
         fclose(src_stream);
-        print_error(reverse_file_path);
         return ERROR;
     }
 
     file_size = get_file_size(src_stream);
     if (file_size == ERROR) {
-        fclose(src_stream);
-        fclose(dest_stream);
-        print_error(file_path);
+        perror(file_path);
+        close_file_streams(dest_stream, src_stream);
         return ERROR;
     }
 
@@ -198,36 +248,35 @@ int create_reverse_file(const char *dest_working_dir, const char *src_working_di
         long offset = max(0, file_size - reversed - BUFFER_MAX);
         size_t count = file_size - offset - reversed;
 
-        if (fseek(src_stream, offset, SEEK_SET) == ERROR) {
-            fclose(src_stream);
-            fclose(dest_stream);
-            print_error(file_path);
+        ret = fseek(src_stream, offset, SEEK_SET);
+        if (ret == ERROR) {
+            perror(file_path);
+            close_file_streams(dest_stream, src_stream);
             return ERROR;
         }
 
         memset(buffer, 0, BUFFER_MAX);
 
-        if (read_file(buffer, count, src_stream) == ERROR) {
-            fclose(src_stream);
-            fclose(dest_stream);
-            print_error(file_path);
+        ret = read_file(buffer, count, src_stream);
+        if (ret == ERROR) {
+            perror(file_path);
+            close_file_streams(dest_stream, src_stream);
             return ERROR;
         }
 
-        reverse_string(buffer, buffer);
+        reverse_string(buffer, buffer, BUFFER_MAX);
 
-        if (write_file(buffer, count, dest_stream) == ERROR) {
-            fclose(src_stream);
-            fclose(dest_stream);
-            print_error(reverse_file_path);
+        ret = write_file(buffer, count, dest_stream);
+        if (ret == ERROR) {
+            perror(reverse_file_path);
+            close_file_streams(dest_stream, src_stream);
             return ERROR;
         }
 
         reversed += count;
     }
 
-    fclose(src_stream);
-    fclose(dest_stream);
+    close_file_streams(dest_stream, src_stream);
 
     return SUCCESS;
 }
