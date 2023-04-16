e44966f1379a411e278d85fe93a46e15061fb4e4
ptrvsrg <s.petrov1@g.nsu.ru>
2023-04-17 01:00:40 +0700

    Inline array

--- files_and_file_systems/task2/create_hard_links.sh
+++ files_and_file_systems/task2/create_hard_links.sh
@@ -3,7 +3,8 @@
 programs=("create_dir" "list_dir_content" "remove_dir" "create_file" "display_file_content" "remove_file" "create_symbolic_link" "display_symbolic_link" "display_file_by_symbolic_link" "remove_symbolic_link" "create_hard_link" "remove_hard_link" "display_file_permissions" "change_file_permissions")
 
 mkdir programs
-for program in "${programs[@]}"  ;
+
+for program in "create_dir" "list_dir_content" "remove_dir" "create_file" "display_file_content" "remove_file" "create_symbolic_link" "display_symbolic_link" "display_file_by_symbolic_link" "remove_symbolic_link" "create_hard_link" "remove_hard_link" "display_file_permissions" "change_file_permissions"
 do
     ln ./task2 programs/$program
 done
